f0f361182976f788f44394e7625d0a8a3c9952f5
Abstrekt <saammmmmmy@gmail.com>
2021-03-04 20:22:11 +0530

    initial

---
+++ pages/jokes/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+      Jokes Page
+    </h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Jokes",
+      meta: [
+        {
+          hid: "",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
